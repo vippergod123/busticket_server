495af2150567731232ba4195184e70383d254844
Cris <duytruong0702@gmail.com>
2019-01-09 15:43:40 +0700

    DUy fix bug in signin

--- routes/Customer/Authentication/signin.js
+++ routes/Customer/Authentication/signin.js
@@ -32,6 +32,7 @@ router.post('/', function(req, res, next) {
             return res.status(200).json({
                 message: "Login Success!",
                 redirect: "/",
+                authProfile: user,
             })
         }
         else { 
